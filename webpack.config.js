b70411fc676a91ae79530f0c23cd18e5f7e75f78
unknown <blaze.poduction@mail.ru>
2020-07-31 18:12:27 +0600

    FInish css layout

--- webpack.config.js
+++ webpack.config.js
@@ -3,25 +3,28 @@ const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const jsLoaders = () => {
-  const loaders = [
-      {
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env']
-      }
-    }
-  ]
-  if(isDev){
-    loaders.push('eslit-loader')
-  }
-  return loaders
-}
 
 const isProd = process.env.NODE_ENV === 'production'
 const isDev = !isProd
 
 const filename = ext => isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`
+
+const jsLoaders = () => {
+  const loaders = [
+    {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env']
+      }
+    }
+  ]
+
+  if (isDev) {
+    loaders.push('eslint-loader')
+  }
+
+  return loaders
+}
 
 module.exports = {
   context: path.resolve(__dirname, 'src'),
@@ -67,7 +70,13 @@ module.exports = {
       {
         test: /\.s[ac]ss$/i,
         use: [
-          MiniCssExtractPlugin.loader,
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: isDev,
+              reloadAll: true
+            }
+          },
           'css-loader',
           'sass-loader'
         ],
